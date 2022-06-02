605aa1d2bc6598d26938faa6fbbf97cec6ef23e7
Flickza <adelholmg@gmail.com>
2022-06-03 00:05:03 +0200

    Works but far from complete

--- popup.js
+++ popup.js
@@ -1,21 +1,110 @@
-
-const pathname = window.location.pathname.split("/");
-const username = pathname[2];
-const user_id = pathname[3];
-
-$.getJSON(`https://www.gamer.no/api/paradise/user/${user_id}/matches/maps`, (data) => {
-    const first_page = 1;
-    const last_page = data.last_page;
-    let userData = [];
-    for (let i = first_page; i < last_page; i++) {
-        userData.push(getUserData(user_id, i));
-    }
-    Promise.all(userData).then((values) => {
-        console.log(values);
+const getUserData = async (id, page) => {
+    return await $.getJSON(`https://www.gamer.no/api/paradise/user/${id}/matches/maps?page=${page}`, (data) => {
+        return data;
     });
-});
-
-const getUserData = (id, page) => {
-    return $.getJSON(`https://www.gamer.no/api/paradise/user/${id}/matches/maps?page=${page}`, (data) => { return data });
 }
 
+const getObjectUser = (id) => {
+    $.getJSON(`https://www.gamer.no/api/paradise/user/${id}/matches/maps`, async (data) => {
+        const first_page = 1;
+        let last_page = data.last_page;
+        let tableObject = [];
+        let userData = [];
+
+        for (let i = first_page; i < last_page + 1; i++) {
+            userData.push(getUserData(id, i));
+        }
+        Promise.all(userData).then((values) => {
+            values.map(data => data.data).forEach(data => {
+                data.forEach(match => {
+                    let map = match.map.resource.name;
+                    let kills = match.kills;
+                    let deaths = match.deaths;
+                    let adr = match.adr;
+                    let rating = match.rating;
+                    if (!tableObject.hasOwnProperty(map)) {
+                        tableObject[map] = {
+                            kills: 0,
+                            deaths: 0,
+                            adr: 0,
+                            rating: 0,
+                            gamesTracked: 0
+                        }
+                    }
+                    tableObject[`${map}`].gamesTracked += 1;
+                    tableObject[`${map}`].kills += kills;
+                    tableObject[`${map}`].deaths += deaths;
+                    tableObject[`${map}`].adr += parseFloat(adr);
+                    tableObject[`${map}`].rating += parseFloat(rating);
+                });
+            });
+
+            Promise.all(Object.keys(tableObject).map(key => {
+                return {
+                    map: key,
+                    kills: tableObject[key].kills,
+                    deaths: tableObject[key].deaths,
+                    kd: tableObject[key].kills / tableObject[key].deaths,
+                    adr: tableObject[key].adr / tableObject[key].gamesTracked,
+                    rating: tableObject[key].rating / tableObject[key].gamesTracked,
+                    gamesTracked: tableObject[key].gamesTracked
+                }
+            })).then(data => {
+                data.sort((a, b) => {
+                    return b.rating - a.rating;
+                });
+                $("#table-body").empty();
+                data.forEach(match => {
+                    $('#table-body').append(`
+                        <tr>
+                            <td>${match.map}</td>
+                            <td>${match.kills}</td>
+                            <td>${match.deaths}</td>
+                            <td>${match.kd.toFixed(2)}</td>
+                            <td>${match.adr.toFixed(2)}</td>
+                            <td>${match.rating.toFixed(2)}</td>
+                            <td>${match.gamesTracked}</td>
+                        </tr>
+                        `);
+                });
+            });
+        });
+    })
+}
+
+
+let pathname = window.location.pathname.split("/");
+if (pathname[1] === "brukere" && pathname[4] === "statistikk") {
+    const username = pathname[2];
+    const user_id = pathname[3];
+
+    if (username && user_id) {
+        $(".user-profile div.mb-4 div.bg-light").html(`
+        <table id="table" class="table table-dark table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Map</th>
+                    <th scope="col">Kills</th>
+                    <th scope="col">Deaths</th>
+                    <th scope="col">K/D</th>
+                    <th scope="col">ADR</th>
+                    <th scope="col">Rating</th>
+                    <th scope="col">Games</th>
+                </tr>
+            </thead>
+            <tbody id="table-body">
+                <tr>
+                    <td>LOADING...</td>
+                    <td>LOADING...</td>
+                    <td>LOADING...</td>
+                    <td>LOADING...</td>
+                    <td>LOADING...</td>
+                    <td>LOADING...</td>
+                    <td>LOADING...</td>
+                </tr>
+            </tbody>
+        </table>`);
+        getObjectUser(user_id);
+    }
+
+}
